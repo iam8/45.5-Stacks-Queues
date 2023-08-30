acd70e3ce1f81f4dc210cbd0868fb1261593a41f
alexm <alex-m@outlook.com>
2023-08-30 15:33:40 -0700

    Add logic to peek() method

--- queue.js
+++ queue.js
@@ -54,7 +54,8 @@ class Queue {
 
     /** peek(): return the value of the first node in the queue. */
     peek() {
-
+        if (!this.first) return null;
+        return this.first.val;
     }
 
     /** isEmpty(): return true if the queue is empty, otherwise false */
