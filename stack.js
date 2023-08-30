cd1fff207ac5fb7ff17891b65c4f2508e1faf3cf
alexm <alex-m@outlook.com>
2023-08-30 15:36:14 -0700

    Adjust spacing and indentation

--- stack.js
+++ stack.js
@@ -1,46 +1,43 @@
-/** Node: node for a stack. */
 
+/** Node: node for a stack. */
 class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
 }
+
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
-
 class Stack {
-  constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
-  }
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+    /** push(val): add new value to end of the stack. Returns undefined. */
+    push(val) {
 
-  push(val) {
+    }
 
-  }
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
+    pop() {
 
-  /** pop(): remove the node from the top of the stack
-   * and return its value. Should throw an error if the stack is empty. */
+    }
 
-  pop() {
+    /** peek(): return the value of the first node in the stack. */
+    peek() {
 
-  }
+    }
 
-  /** peek(): return the value of the first node in the stack. */
+    /** isEmpty(): return true if the stack is empty, otherwise false */
+    isEmpty() {
 
-  peek() {
-
-  }
-
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
-  isEmpty() {
-
-  }
+    }
 }
+
 
 module.exports = Stack;
